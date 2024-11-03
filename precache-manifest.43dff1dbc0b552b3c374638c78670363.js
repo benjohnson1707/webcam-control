8fd2eb7e22818ee1578e51c3428185b1c1bd0531
Your Name <youremail@example.com>
2024-11-03 16:14:46 +0100

    Updates

---
+++ precache-manifest.43dff1dbc0b552b3c374638c78670363.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e638c345d9392848b0c6717d0a2eb5b",
+    "url": "/benjohnson1707/webcam-control.git/index.html"
+  },
+  {
+    "revision": "e67e1848340ce2f12642",
+    "url": "/benjohnson1707/webcam-control.git/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "17ce46f434265d83d8ad",
+    "url": "/benjohnson1707/webcam-control.git/static/js/2.e951d4e8.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/benjohnson1707/webcam-control.git/static/js/2.e951d4e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f6b689b3435993093a",
+    "url": "/benjohnson1707/webcam-control.git/static/js/3.331ae4c8.chunk.js"
+  },
+  {
+    "revision": "e67e1848340ce2f12642",
+    "url": "/benjohnson1707/webcam-control.git/static/js/main.62c96968.chunk.js"
+  },
+  {
+    "revision": "927fc383f6c44eae17f9",
+    "url": "/benjohnson1707/webcam-control.git/static/js/runtime-main.a0969e24.js"
+  }
+]);
